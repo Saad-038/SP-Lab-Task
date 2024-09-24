a7bd592c652d998adf94817a8dbe6e1f48fbd38e
Sazzad Rashid Chowdhury <175148215+Saad-038@users.noreply.github.com>
2024-09-24 23:45:59 +0600

    Sp Lab Task-3

---
+++ Sp lab Task-3.c
@@ -0,0 +1,34 @@
+// finding the largest of three numbers using nested if-else conditions.
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter 3 numbers = ");
+    scanf("%d %d %d",&a,&b,&c);
+
+     if (a>=b)
+        {
+        if (a>=b)
+        {
+            printf("Largest number is = %d\n", a);
+        }
+     else
+        {
+            printf("Largest number is = %d\n", c);
+        }
+    }
+    else
+        {
+        if (b>=c)
+        {
+            printf("Largest number is = %d\n", b);
+        }
+    else
+        {
+            printf("Largest number is = %d\n", c);
+        }
+    }
+
+    return 0;
+}
+
