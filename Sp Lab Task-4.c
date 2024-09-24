f371c4f0c92ac93945501e96d828a821b0ef9c70
Sazzad Rashid Chowdhury <175148215+Saad-038@users.noreply.github.com>
2024-09-25 01:08:39 +0600

    Sp Lab Task-4

---
+++ Sp Lab Task-4.c
@@ -0,0 +1,34 @@
+//taking a score as input and assigns a grade (A, B, C, D, F) using the switch-case statement.
+#include<stdio.h>
+int main()
+{
+    int marks;
+    printf("enter your marks (0-100)\n");
+    scanf("%d",&marks);
+
+    switch(marks/10)
+    {
+    case 10:
+    case 9:
+    printf("grade is A\n");
+    break;
+
+    case 8:
+    printf("grade is B\n");
+    break;
+
+    case 7:
+    printf("grade is C\n");
+    break;
+
+    case 6:
+    printf("grade is D\n");
+    break;
+
+    default:
+    printf("grade is F\n");
+
+    }
+    return 0;
+}
+
