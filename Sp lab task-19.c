8f07b318a66de793ff5c3acfe241cddaaad3c01a
Sazzad Rashid Chowdhury <175148215+Saad-038@users.noreply.github.com>
2024-09-27 17:14:44 +0600

    Sp lab task-19

---
+++ Sp lab task-19.c
@@ -0,0 +1,18 @@
+//Write a program to reverse the elements of an array using a loop
+#include<stdio.h>
+int main()
+{
+    int arr[5],i;
+    printf("enter elements of array = ");
+    for(i=0;i<=4;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Reverse array elements = \n");
+    for(i=4;i>=0;i--)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
+
